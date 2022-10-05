a8341281385b299a527cd3f4924263ea63d6709a
hwang6291 <bloghost.angela@gmail.com>
2022-10-05 10:53:53 +0800

    20221005
    https://github.com/hwang6291/bswebsite/raw/gh-pages/mgzapp.exe

---
+++ js/dwlargefile.js
@@ -0,0 +1,9 @@
+$(function(){
+    function downloadWithProgress() {
+        const startTime = new Date().getTime();
+
+        request = new XMLHttpRequest();
+        request.responseType = "blob";
+        
+    }
+})
